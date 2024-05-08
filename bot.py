98a481f256c4889b6a69ddcb72e70565d310ec90
Bot-Bucket <163278950+Bot-Bucket@users.noreply.github.com>
2024-05-08 19:40:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ class Bot(Client):
             bot_token=BOT_TOKEN,
             workers=500,
             plugins={"root": "plugins"},
-            sleep_threshold=5,
+            sleep_threshold=1954,
         )
 
     async def start(self):
