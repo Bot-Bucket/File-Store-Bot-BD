2d01c83481f7d2cb0d24862f070722cac987182f
Bot-Bucket <163278950+Bot-Bucket@users.noreply.github.com>
2024-05-08 16:38:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/Bot-Bucket/File-Store-Bot-BD /File-Store-Bot-BD
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /File-Store-Bot-BD
 fi
-cd /Advance-File-Share-Bot
+cd /File-Store-Bot-BD
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
