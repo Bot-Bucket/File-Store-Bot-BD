5fb5eab1ffa06e58e76d499df819682785602ea4
Bot-Bucket <163278950+Bot-Bucket@users.noreply.github.com>
2024-05-08 20:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Bot-Bucket'
+def hello():
+    return 'Hello, World!'
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    # Use the PORT environment variable provided by Heroku,
+    # or default to 5000 if running locally.
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
